89ffcda39ca10f02440f4c7c2bc8252b6faf5a2f
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-01-31 06:57:30 -0300

    fetch/custom hook

--- hora-de-codar/6_HTTP_REACT/http-react/src/App.jsx
+++ hora-de-codar/6_HTTP_REACT/http-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useFetch } from "./hooks/useFetch";
 
 const url = "http://localhost:3000/products";
 
@@ -8,29 +9,84 @@ function App() {
   // Resgatando dados
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch(url);
-      const data = await res.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
 
-      setProducts(data);
+  //     setProducts(data);
+  //   }
+
+  //   getData();
+  // }, []);
+
+  // Custom hook
+
+  const {data: items} = useFetch(url);
+
+  // Envio de dados
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
     }
 
-    getData();
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product)
+    })
 
-  }, []);
+    const addedProduct = await res.json();
+    setProducts((prevProducts) => [...products, addedProduct]);
+  }
+
+
 
   return (
     <>
       <h1>HTTP em React</h1>
 
       <ul>
-        {products.map((product) => (
+        {items &&
+        items.map((product) => (
           <li key={product.id}>
             {product.name} - R${product.price}
           </li>
         ))}
       </ul>
+
+      {/* Envio de dados */}
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome</span>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Preço</span>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </>
   );
 }
